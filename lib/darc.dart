d1209f8eeb11d3f00efe14a823d5fa71c33d726c
Edward Ogolah <80966829+darcishED@users.noreply.github.com>
2025-05-24 06:06:56 +0300

    What just happened?

--- lib/darc.dart
+++ lib/darc.dart
@@ -41,7 +41,6 @@ class DarC {
     }
 
     // Initialize Supabase - only once
-<<<<<<< HEAD
     if (_supabaseClientInstance == null) {
       try {
         await Supabase.initialize(url: supabaseUrl, anonKey: supabaseKey);
@@ -49,13 +48,6 @@ class DarC {
       } on AssertionError catch (e) {
         debugPrint(e.message);
       }
-=======
-    try {
-      _supabaseClientInstance = supabase;
-    } on AssertionError catch (e) {
-      await Supabase.initialize(url: supabaseUrl, anonKey: supabaseKey);
-      _supabaseClientInstance = supabase;
->>>>>>> 4bde1f72bba683272c9ef5bd3087bb3ce06143fa
     }
 
     // Initialize Firebase
